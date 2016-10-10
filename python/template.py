06cc62458ff673d74af820c1e9d00c3ac5609580
Takuma Okamoto <takuma_okamoto@metaps.com>
2016-10-10 13:33:56 +0900

    Add template

---
+++ python/template.py
@@ -0,0 +1,15 @@
+import math
+import time
+
+def compute():
+    return 0
+
+if __name__ == "__main__":
+    print("Start")
+    print("----------------------")
+    start = time.time()
+    answer = compute()
+    end = time.time()
+    print("----------------------")
+    print("Answer : ", answer)
+    print("End ", end - start, "sec")
