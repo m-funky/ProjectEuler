b32253df7f8834f361fcacbdfce71a8a722aed82
Takuma Okamoto <takuma_okamoto@metaps.com>
2016-10-16 20:21:15 +0900

    Add p034

---
+++ python/p034.py
@@ -0,0 +1,33 @@
+import math
+import time
+
+# if n < 7, 10 ** n - 1 < 9! * n
+# but 7 <= n, 10 ** n - 1 > 9! * n
+# so finb until 2540160 = 9! * 7
+
+FACTORIALS = [ math.factorial(x) for x in range(0, 10)]
+MAX = FACTORIALS[9] * 7
+
+def compute():
+
+    total = 0
+
+    for i in range(3, MAX):
+        print(i)
+        sum_of_factorial = sum([ FACTORIALS[int(n)] for n in list(str(i))])
+        if i == sum_of_factorial:
+            print('ANS', i)
+            total += i
+
+
+    return total
+
+if __name__ == "__main__":
+    print("Start")
+    print("----------------------")
+    start = time.time()
+    answer = compute()
+    end = time.time()
+    print("----------------------")
+    print("Answer : ", answer)
+    print("End ", end - start, "sec")
