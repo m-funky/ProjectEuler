7a1292fab7093cc1b1ae74a906c4ba154b152909
Takuma Okamoto <takuma_okamoto@metaps.com>
2016-11-13 16:06:32 +0900

    Solve 084

--- python/p084.py
+++ python/p084.py
@@ -1,61 +1,103 @@
+# calculate by simulation
+
 import math
 import time
-
-squares = [0 for x in range(40)]
-dices = {x:0 for x in range(24)} # max is 23, 4 dobule twice, and 4 + 3
+import random
 
 GO = 0
 GAIL = 10
+G2J = 30
 CC = [2, 17, 33]
 CH = [7, 22, 36]
 
 
+DICE_MAX = 4
+
+cc_rand = 0
+ch_rand = 0
+
+
 def compute():
-    calculate_dice_prob()
 
-    for n, p in dices.items():
-        if n == 0:
-            squares[GAIL] += p
-        elif n in CC:
-            squares[GAIL] += p * (1 / 16)
-            squares[GO] += p * (1 / 16)
-            squares[n] += p * (10 / 16)
-        elif n in CH:
-            squares[GAIL] += p * (1 / 16)
-            squares[GO] += p * (1 / 16)
-            squares[11] += p * (1 / 16)
-            squares[24] += p * (1 / 16)
-            squares[39] += p * (1 / 16)
-            squares[5] += p * (1 / 16)
-            if n == 7:
-                squares[5] += p * (2 / 16) # R1
-                squares[12] += p * (1 / 16) # U1
-            elif n == 22:
-                squares[25] += p * (2 / 16) # R2
-                squares[28] += p * (1 / 16) # U1
+    total_squares = [0 for x in range(40)]
+    dices = [0 , 0, 0, 0, 0, 0, 0]
 
-            squares[n - 3] += p * (1 / 16)
-            squares[n] += p * (6 / 16)
+    n = 0
+    double = 0
+    for x in range(1000000):
+        i = random.randint(1, DICE_MAX)
+        j = random.randint(1, DICE_MAX)
+
+        if i == j:
+            double += 1
         else:
-            squares[n] += p
+            double = 0
 
-    return squares
-    #return [squares.index(p) for p in sorted(squares)[-3:]]
+        if double > 2:
+            n = GAIL
+            double = 0
+        else:
+            n = square_by_dice_sum(n, i + j)
 
-def calculate_dice_prob(prev_sum=0, loop=0):
-    for i in range(1, 5):
-        for j in range(1, 5):
-            if i == j:
-                if loop == 2:
-                    dices[0] += (1 / 16) * (1 / 16) ** loop #  0 means GAIL
-                else:
-                    calculate_dice_prob(i + j + prev_sum, loop + 1)
-            else:
-                dices[i + j + prev_sum] += (1 / 16) * (1 / 16) ** loop
+        total_squares[n] += 1
 
 
+    print({x:n for x, n in enumerate(total_squares)})
+
+    return [total_squares.index(p) for p in sorted(total_squares)[-1:-4:-1]]
 
 
+def square_by_dice_sum(start, dice_sum):
+    global cc_rand, ch_rand
+
+    n = (start + dice_sum) % 40
+
+    if n == G2J:
+        return GAIL;
+    if n in CC:
+        cc_rand = (cc_rand + 1) % 16
+        rand = cc_rand
+        if rand == 1:
+            return GAIL
+        elif rand == 2:
+            return GO
+        else:
+            return n
+    if n in CH:
+        ch_rand = (ch_rand + 1) % 16
+        rand = ch_rand
+
+        if rand == 1:
+            return GAIL
+        elif rand == 2:
+            return GO
+        elif rand == 3:
+            return 11
+        elif rand == 4:
+            return 24
+        elif rand == 5:
+            return 39
+        elif rand == 6:
+            return 5
+        elif rand in [7, 8]:
+            if n == 7:
+                return 15
+            elif n == 22:
+                return 25
+            elif n == 36:
+                return 5
+        elif rand == 9:
+            if n == 7:
+                return 12
+            elif n == 22:
+                return 28
+            elif n == 36:
+                return 12
+
+        elif rand == 10:
+            return square_by_dice_sum((n - 3) % 40, 0) # CH3 -> CC3
+
+    return n
 
 
 if __name__ == "__main__":
