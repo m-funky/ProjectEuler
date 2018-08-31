fc6c3f03a2d65656a0b9293a4973844682d1e387
Takuma Okamoto <takuma.okamoto.35@gmail.com>
2018-08-31 10:17:09 +0900

    Add comment

--- python/p088.py
+++ python/p088.py
@@ -12,6 +12,7 @@ def compute():
         num_sum = k
         while True:
             num_sum += 1
+
             num_lists = num_lists_from_sum(k, num_sum)
 
             has_num = False
@@ -32,6 +33,7 @@ def compute():
     return num_set
 
 
+# num_lists_from_sum(5, 8) = [[4, 1, 1, 1, 1], [3, 2, 1, 1, 1], [2, 2, 2, 1, 1]]
 def num_lists_from_sum(k, num_sum):
 
     if k == 1:
@@ -50,6 +52,7 @@ def num_lists_from_sum(k, num_sum):
     return num_lists
 
 
+# prodcut(3) = 1 * 2 * 3 = 6
 def product(num_list):
     num = 1
     for x in num_list:
@@ -66,5 +69,5 @@ if __name__ == "__main__":
     answer = compute()
     end = time.time()
     print("----------------------")
-    print("Answer : ", answer)
+    print("Answer : ", sum(answer), answer)
     print("End ", end - start, "sec")
