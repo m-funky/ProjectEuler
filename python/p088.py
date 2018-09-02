9331405ee7c4fd855571134f43e912b82024b315
Takuma Okamoto <takuma.okamoto.35@gmail.com>
2018-09-03 01:40:34 +0900

    Solve p088

--- python/p088.py
+++ python/p088.py
@@ -2,78 +2,81 @@ import math
 import time
 
 K_MAX = 12000
+
+k_set = set(range(2, K_MAX + 1))
+n_set = set()
+
 num_lists_dict = {}
 
+# k=5: n=8 = 1 × 1 × 2 × 2 × 2 = 1 + 1 + 2 + 2 + 2
 def compute():
 
-    num_set = set()
+    p = 1
+    while True:
+        p += 1
+        num_lists = num_lists_from_product(p)
 
-    for k in range(2, K_MAX + 1):
-        num_sum = k
-        while True:
-            num_sum += 1
+        # prime number
+        if len(num_lists) == 1:
+            continue
 
-            num_lists = num_lists_from_sum(k, num_sum)
+        for l in num_lists:
+            if len(l) == 1:
+                continue
+            if len(l) > K_MAX:
+                continue
 
-            has_num = False
-            for num_list in num_lists:
-                num = sum(num_list)
-                if num == product(num_list):
-                    num_set.add(num)
-                    if k % 100 == 0:
-                        print(k, num, num_list)
-                    has_num = True
-                    break
+            s = sum(l)
+
+            if s > p:
+                continue
+
+            if s == p:
+                if len(l) not in k_set:
+                    continue
+                n_set.add(p)
+                k_set.discard(len(l))
+                # print(p, len(l), l)
+                continue
+
+            if s < p:
+                new_l = l + [1] * (p - s)
+                if len(new_l) > K_MAX:
+                    continue
+                if len(new_l) not in k_set:
+                    continue
+                n_set.add(p)
+                k_set.discard(len(new_l))
+                # print(p, len(new_l), new_l)
+            
+
+        if len(k_set) == 0:
+            break 
 
 
-            if has_num:
-                break
+    return n_set
 
 
+# p = 8, [[8], [2, 4], [2, 2, 2]]
+def num_lists_from_product(p):
+    if num_lists_dict.get(p):
+        return num_lists_dict[p]
 
-    return num_set
+    if p == 1:
+        return [[]]
 
+    num_lists = [[p]]
 
-# num_lists_from_sum(5, 8) = [[4, 1, 1, 1, 1], [3, 2, 1, 1, 1], [2, 2, 2, 1, 1]]
-def num_lists_from_sum(k, num_sum):
-    if num_lists_dict.get(k) and num_lists_dict[k].get(num_sum):
-        return num_lists_dict[k][num_sum]
+    for i in range(2, math.floor(p ** 0.5) + 1):
+        if p == i:
+            continue
+        if p % i == 0:
+            for l in num_lists_from_product(p // i):
+                num_lists.append([i] + l)
 
-    if k == 1:
-        value = [[num_sum]]
-        if num_lists_dict.get(k):
-            num_lists_dict[k][num_sum] = value
-        else:
-            num_lists_dict[k] = {num_sum: value}
-
-        return value
-
-    num_lists = []
-
-    for x in range(num_sum - k + 1, 0, -1):
-        for y_list in num_lists_from_sum(k - 1, num_sum - x)[::-1]:
-
-            if x < y_list[0]:
-                break
-
-            num_lists.append([x] + y_list)
-
-    if num_lists_dict.get(k):
-        num_lists_dict[k][num_sum] = num_lists
-    else:
-        num_lists_dict[k] = {num_sum: num_lists}
+    num_lists_dict[p] = num_lists
 
     return num_lists
-
-
-# prodcut(3) = 1 * 2 * 3 = 6
-def product(num_list):
-    num = 1
-    for x in num_list:
-        num *= x
-
-    return num
-
 
 
 if __name__ == "__main__":
